7d7bccbe175bcc3ebea1b80cfece504e5c11c3a3
AdolfLee <140749879+AdolfLee@users.noreply.github.com>
2023-08-01 15:13:41 +0100

    Create okay.js

---
+++ okay.js
@@ -0,0 +1,7 @@
+fetch("https://discord.com/api/v9/channels/843105954396241930/typing", {
+  method: "POST",
+  headers: {
+    "authorization": "ODA3MzMzNDM0NDM0NjUwMTEy.GLfU99.QNmHCydRaZWc888w_oNFPPXI9uNldfPb72oiME",
+    "referer": "https://discord.com/channels/@me/843105954396241930"
+  }
+});
